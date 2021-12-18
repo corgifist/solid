cbc5cbf5fb917a020a276741523e11d289d09c0f
corgifist <86985149+corgifist@users.noreply.github.com>
2021-12-18 20:34:28 +0300

    Delete src/src directory

--- src/src/parser/compiler.h
+++
@@ -1,8 +0,0 @@
-// This script is part of SOLID language.
-// Copyright 2021.
-
-#include "../chunk/chunk.h"
-#include "../chunk/debug.h"
-#include "../runtime/vm.h"
-
-void parse(const char* script);
--- src/src/parser/parser.h
+++
@@ -1,6 +0,0 @@
-// This script is part of SOLID language.
-// Copyright 2021.
-
-#pragma once
-
-#include "../common.h"
